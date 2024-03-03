0280bf749e785787316bf4279bafa94a94641217
Nicholas Reiss <njreiss33@gmail.com>
2024-03-03 04:18:15 -0500

    trying a regular push

--- src/App.js
+++ src/App.js
@@ -24,12 +24,12 @@ const App = () => {
       <div className="flex absolute items-center top-0 w-full h-20">
         <div className='flex-row w-full'>
           <div className='flex px-10'>
-            <div className='text-5xl text-textBlue'>
+            <div className='text-4xl text-textBlue'>
               FlightFinder
             </div>
             <div className="ml-auto text-xl">
-              <button className='border-bgBlue text-tertiary p-2 rounded-2xl border-4'>
-                sign in
+              <button className='border-bgBlue text-textBlue px-2 py-1 rounded-xl border-4'>
+                Sign In
               </button>
             </div>
           </div>
@@ -38,8 +38,8 @@ const App = () => {
       </div>
       
       {/*Conversation type area */}
-      <div className='w-5/6 pt-24'>
-        <div className='w-fit max-w-prose text-xl text-textBone px-3 py-2 float-right bg-bgBlue rounded-xl'>
+      <div className='w-[40%] pt-[88px]'>
+        <div className='max-w-96 w-fit text-md text-textBone px-4 py-2 float-right bg-bgBlue rounded-xl'>
           {floatingText && (
             <p className="floating-text transition-all duration-500 ease-in-out">
               {floatingText}
@@ -49,10 +49,10 @@ const App = () => {
       </div>
 
       {/*Bottom of the page: vertical line and input prompt */}
-      <div className="flex flex-col absolute bottom-0 w-5/6 h-20 items-center ">
-        <hr className='w-5/6 border-1 border-bgBlue'/>
+      <div className="flex flex-col absolute bottom-0 w-[40%] h-18 items-center">
+        <hr className='opacity-50 w-[-4] border-1 border-bgBlue' style={{ width: "calc(100% - 20px)" }}/>
         <input 
-          className='flex-grow w-full rounded-2xl border-4 bg-bgBone border-bgBlue text-xl placeholder-tertiary text-textBlue m-3 px-3 py-2 focus:outline-none' 
+          className='flex-grow w-full rounded-2xl border-4 bg-bgBone border-bgBlue text-md placeholder-tertiary text-textBlue mx-3 mb-3 mt-1 px-3 py-2 focus:outline-none' 
           name="textfield"
           placeholder='Write a prompt to begin searching flights'
           value={inputValue}
